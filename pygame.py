fa6505e1435466a722b7a51b334e898ee055b0a7
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-13 23:41:30 -0800

    updated pygame

--- pygame.py
+++ pygame.py
@@ -4,6 +4,22 @@ pygame.init()
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
 
+# Define GameObject class
+class GameObject(pygame.sprite.Sprite):
+  def __init__(self, x, y, width, height):
+    super(GameObject, self).__init__()
+    self.surf = pygame.Surface((width, height))
+    self.surf.fill((255, 0, 255))
+    self.rect = self.surf.get_rect()
+    self.x = x
+    self.y = y
+
+  def render(self, screen):
+    screen.blit(self.surf, (self.x, self.y))
+
+# Instance of GameObject
+box = GameObject(120, 300, 50, 50)
+
 # Create the game loop
 running = True
 while running:
@@ -11,15 +27,19 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+
+  # Clear screen
+  screen.fill((255, 255, 255))
       
   # Draw a circle
-  screen.fill((255, 255, 255))
   pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+  # Render game object
+  box.render(screen)
+
   # Update the window
   pygame.display.flip()
 
-  # Render game object
-  game_object.render(screen)
 
 # Quit pygame
 
