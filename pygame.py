19b10b0ab52e6b0461dd1e52a3ede87b947c04d1
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-13 23:05:22 -0800

    updated pygame

--- pygame.py
+++ pygame.py
@@ -3,6 +3,19 @@ import pygame
 pygame.init()
 # Configure the screen
 screen = pygame.display.set_mode([500, 500])
+
+class GameObject(pygame.sprite.Sprite):
+  def __init__(self, x, y, width, height):
+    super(GameObject, self).__init__()
+    self.surf = pygame.Surface((width, height))
+    self.surf.fill((255, 0, 255))
+    self.rect = self.surf.get_rect()
+    self.x = x
+    self.y = y
+
+  def render(self, screen):
+    screen.blit(self.surf, (self.x, self.y))
+
 # Create the game loop
 running = True
 while running:
