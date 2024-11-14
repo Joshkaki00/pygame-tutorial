29043ef7ab08c9fa80ef8d261b6b800e74ee4147
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-13 23:38:16 -0800

    updated pygame

--- pygame.py
+++ pygame.py
@@ -7,17 +7,6 @@ screen = pygame.display.set_mode([500, 500])
 # Create the game loop
 running = True
 while running:
-  class GameObject(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
-      super(GameObject, self).__init__()
-      self.surf = pygame.Surface((width, height))
-      self.surf.fill((255, 0, 255))
-      self.rect = self.surf.get_rect()
-      self.x = x
-      self.y = y
-
-    def render(self, screen):
-      screen.blit(self.surf, (self.x, self.y))
   # Looks at events
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
