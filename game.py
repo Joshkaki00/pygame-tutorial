43a04206734488146d089c0bcb117cc1b2fd6ac3
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-13 23:58:02 -0800

    updated pygame

--- game.py
+++ game.py
@@ -6,20 +6,20 @@ pygame.init()
 screen = pygame.display.set_mode([500, 500])
 
 # Define GameObject class
+# Game Object
 class GameObject(pygame.sprite.Sprite):
-  def __init__(self, x, y, width, height):
+  # Remove width and height and add image here!
+  def __init__(self, x, y, image):
     super(GameObject, self).__init__()
-    self.surf = pygame.Surface((width, height))
-    self.surf.fill((255, 0, 255))
-    self.rect = self.surf.get_rect(topleft=(x, y))
+    self.surf = pygame.image.load(image) # ADD!
     self.x = x
     self.y = y
 
   def render(self, screen):
-    screen.blit(self.surf, self.rect.topleft)
+    screen.blit(self.surf, (self.x, self.y))
 
 # Instance of GameObject
-box = GameObject(120, 300, 50, 50)
+apple = GameObject(120, 300, 'apple.png')
 
 # Create the game loop
 running = True
