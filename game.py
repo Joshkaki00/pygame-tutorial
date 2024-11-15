54b09a7369308f5f702211e3e20192baf5ca131c
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2024-11-15 13:27:07 -0800

    updated pygame

--- game.py
+++ game.py
@@ -23,7 +23,7 @@ apple_image = 'apple.png'
 strawberry_image = 'strawberry.png'
 
 # Grid setup
-start_x, start_y = 80, 80
+start_x, start_y = 90, 90
 spacing = 140
 
 # List to hold all GameObjects on grid
